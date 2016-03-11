bd77015f2a56d6368d2ce972a5f8dfb2c64ae607
zhangzeyan <1059920861@qq.com>
2016-03-11 17:05:02 +0800

    模块提交  author: simple  date: 16/03/11

---
+++ Main_YueChao_Market/src/main/java/com/ycz/yuechaomarket/activity/ModelActivity.java
@@ -0,0 +1,15 @@
+package com.ycz.yuechaomarket.activity;
+
+import com.ycz.yuechaomarket.R;
+import com.zhy.autolayout.AutoLayoutActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by zhangzeyan on 16/3/11.
+ */
+
+@EActivity(R.layout.yuechao_model)
+public class ModelActivity extends AutoLayoutActivity {
+
+}
